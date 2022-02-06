b72c437409d017df7a6d0f3aa1cdb4ae69e27f54
hackurity01 <hackurity01@gmail.com>
2022-02-06 18:49:16 +0900

    12-4. Custom hook 만들기

---
+++ src/hooks/useCurrentQuestion.js
@@ -0,0 +1,13 @@
+import { useRecoilValue } from 'recoil';
+
+import questionsState from '../stores/questions/atom';
+import useStep from './useStep';
+
+function useCurrentQuestion() {
+  const step = useStep();
+  const questions = useRecoilValue(questionsState);
+
+  return questions[step];
+}
+
+export default useCurrentQuestion;
